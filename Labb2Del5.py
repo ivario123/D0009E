4f4c6b4a7f5c1ea9f0d1cdea7a2adf334106e807
ivario123 <ivajns-9@student.ltu.se>
2020-08-30 17:32:42 +0200

    iuh

--- Labb2Del5.py
+++ Labb2Del5.py
@@ -3,7 +3,7 @@ import random
 #///////////////////////////////////////////////////////////////////////////////////////
 #------------------------------------------.....---------------------------------------
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-derivative =lambda f,x,h: (f(x+h)-f(x-h))/(2*h)
+derivative =lambda f,x,h: 1.0 / (2 * h) * (f(x + h) - f(x - h))
 #region
 #print(derivative(cos,0,0.0001))
 #print(derivative(sin,pi,0.0001))
@@ -13,12 +13,11 @@ derivative =lambda f,x,h: (f(x+h)-f(x-h))/(2*h)
 #------------------------------------------.....---------------------------------------
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 def solve(f,x0,h):
-    x = x0
-    xn = lambda x,h : x-f(x)/derivative(f,x,h)
-    while abs(x-xn(x,h))>h or xn(x,h)==x:
-        x = xn(x,h)
-    return x
-k = lambda x:x**2+2*x+2
-#print(solve(cos,0.0002,0.00001))
-#print(solve(exp,2,0.00001))
-#print(solve(sin,0.0001,0.00001))
+    xn = 0
+    while True:
+        xn = x0 - (f(x0) / derivative(f, x0, h))
+        if abs(xn - x0) < h:
+            return xn
+        x0 = xn
+k = lambda x:x**2+2*x+10
+print(solve(k,-9,0.001))
