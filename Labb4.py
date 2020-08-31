34b554e7a3dbe9b9002081d18212d87cef615c9f
ivario123 <ivajns-9@student.ltu.se>
2020-08-31 14:51:16 +0200

    Slutförde

--- Labb4.py
+++ Labb4.py
@@ -8,50 +8,71 @@ class user:
         self.Nick = nick
         self.Num = num
     def __repr__(self):
-        return num
+        return self.num
     
         
-Phonebook = []
-def add(args):
+
+def add(args,Phonebook):
+    assert len(args)==2
     for el in Phonebook:
         if el.Name==args[0]:
             print('Det finns redan en användare med det namnet\n')
-            return
+        if el.Nick==args[0]:
+            print('Det finns redan en användare med det namnet\n')
+            
     try:
         Phonebook.append(user(args[0],args[0],args[1]))
+        return Phonebook
     except:
         print("Du gav inte tillräckligt många argument för\n lägga till en ny användare")
-def lookup(args):
+        return Phonebook
+    
+def lookup(args,Phonebook):
+    assert len(args)==1
     for el in Phonebook:
         if el.Name == args[0] or el.Nick == args[0]:
-            return el.Num
-    return "Hittade inte den användaren"
-def alias(args):
+            print("{}'s elller om man vill {}'s nummer är : {}".format(el.Name,el.Nick,el.Num))
+            return Phonebook
+    print("Hittade inte den användaren")
+    return Phonebook
+def alias(args,Phonebook):
+    assert len(args)==2
     for el in Phonebook:
         if el.Name == args[0]:
             el.Nick = args[1]
-            return
+            return Phonebook
     print("Den användaren finns inte")
-def pop(args):
+    return Phonebook
+def pop(args,Phonebook):
+    assert len(args)==1
     for el in Phonebook:
         if el.Name == args[0] or el.Nick == args[0]:
             Phonebook.pop(Phonebook.index(el))
             print("\"pop\"")
-            return
-    return "Hittade inte den användaren"
-def loop():
+            return Phonebook
+    print( "Hittade inte den användaren",)
+    return Phonebook
+def TryTask(task,inp,expectedargs):
+    try:
+        p = task(inp[0],inp[1])
+        return p
+    except:
+        print("Du måste ange {} argment separerade med mellanrum".format(expectedargs),)
+        return inp[1]
+def loop(Phonebook):
     arg = input("Telebok > ").split()
     if(arg[0] == "add"):
-        add(arg[1:3])
+        Phonebook = TryTask(add,[arg[1:3],Phonebook],2)
     elif(arg[0]=="lookup"):
-        print(lookup(arg[1:2]),)
+        Phonebook = TryTask(lookup,[arg[1:2],Phonebook],1)
     elif(arg[0]=="alias"):
-        alias(arg[1:3])
+        Phonebook = TryTask(alias,[arg[1:3],Phonebook],2)
     elif(arg[0]=="remove"):
-        pop(arg[1:3])
+        Phonebook = TryTask(pop,[arg[1:2],Phonebook],1)
     else:
         print("Invalid request\n")
-    loop()
+    loop(Phonebook)
 
 if __name__ == "__main__":
-    loop()
+    Phonebook = []
+    loop(Phonebook)
